cb53c2aa43ea63c792ec2b48507d1e832c274835
Sparkleverse <sparkleverse@gmail.com>
2017-01-22 03:43:01 -0700

    Update and rename VayneSkins.lua to SkinHack.lua

--- SkinHack.lua
+++ SkinHack.lua
@@ -7,10 +7,10 @@ function AutoUpdate(data)
     if tonumber(data) > tonumber(ver) then
         print("New version found! " .. data)
         print("Downloading update, please wait...")
-        DownloadFileAsync("https://raw.githubusercontent.com/Sparkleverse/juststuff/master/VayneSkins.lua", SCRIPT_PATH .. "VayneSkins.lua", function() print("Update Complete, please 2x F6!") return end)
+        DownloadFileAsync("https://raw.githubusercontent.com/Sparkleverse/juststuff/master/SkinHack.lua", SCRIPT_PATH .. "SkinHack.lua", function() print("Update Complete, please 2x F6!") return end)
     end
 end
-GetWebResultAsync("https://raw.githubusercontent.com/Sparkleverse/juststuff/master/VayneSkins.version", AutoUpdate)
+GetWebResultAsync("https://raw.githubusercontent.com/Sparkleverse/juststuff/master/SkinHack.version", AutoUpdate)
 
 require ("OpenPredict")
 require ("MapPositionGOS")
@@ -25,8 +25,10 @@ local SkinMenu = Menu("SkinChanger", "SkinChanger")
 
 local skinMeta = {["Bard"] = {"Classic", "Elderwood", "Bloom Marigold", "Bloom Ivy", "Bloom Sage", "Snow Day", "Bard Bard"}, 
     ["Jhin"] = {"Classic", "High Noon"}, 
-    ["Khazix"] = {"Classic", "Mecha", "Guardian of the Sands", "Death Blossom"},
-    ["Vayne"] = {"Classic", "Vindicator", "Aristocrat", "DragonSlayer", "Heartseeker", "SKT T1", "Arclight", "DragonSlayer Green", "DragonSlayer Red", "DragonSlayer Blue", "SoulStealer"}}
+    ["Khazix"] = {"Classic", "Mecha", "GotS (Broken Claws ATM)", "Death Blossom"},
+    ["Vayne"] = {"Classic", "Vindicator", "Aristocrat", "DragonSlayer", "Heartseeker", "SKT T1", "Arclight", "DragonSlayer Green", "DragonSlayer Red", "DragonSlayer Blue", "SoulStealer"},
+    ["Veigar"] = {"Classic", "White Mage", "Curling", "Greybeard", "Leprechaun", "Baron Von", "Superb Villain", "Bad Santa", "Final Boss"},
+    ["Vi"] = {"Classic", "Neon Strike", "Officer", "Debonair", "Demon", "Warring Kingdoms"}}
 
 
 --SkinMenu.SkinChanger
