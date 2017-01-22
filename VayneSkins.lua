520a8a37c24121ce1fc70c8d8de53ead9f68737f
Sparkleverse <sparkleverse@gmail.com>
2017-01-22 01:11:31 -0700

    Update VayneSkins.lua

--- VayneSkins.lua
+++ VayneSkins.lua
@@ -1,4 +1,5 @@
 --if GetObjectName(myHero) ~= "Vayne" then return end
+--if GetObjectName(myHero) == "Vayne" or GetObjectName(myHero) == "Bard" then return end
 
 local ver = "0.02"
 
@@ -19,15 +20,15 @@ require ("ChallengerCommon")
 --[[local SkinMenu = Menu("Vayne", "Vayne")
 --Skin Changer
 SkinMenu:SubMenu("SkinChanger", "SkinChanger")]]
-
 local SkinMenu = Menu("SkinChanger", "SkinChanger")
+
 
 local skinMeta = {["Vayne"] = {"Classic", "Vindicator", "Aristocrat", "DragonSlayer", "Heartseeker", "SKT T1", "Arclight", "DragonSlayer Green", "DragonSlayer Red", "DragonSlayer Blue", "SoulStealer"}}
 --SkinMenu.SkinChanger
-if GetObjectName(myHero) ~= "Vayne" then return
-    SkinMenu:DropDown('skin', myHero.charName.. " Skin List", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
+SkinMenu:DropDown('skin', myHero.charName.. " Skin List", 1, skinMeta[myHero.charName], HeroSkinChanger, true)
 --SkinMenu.SkinChanger
-    SkinMenu.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end	
+if GetObjectName(myHero) == "Vayne" then
+    SkinMenu.skin.callback = function(model) HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") end
 end
 
 --Hope you enjoyed reading this script in github. It's pretty simple.
